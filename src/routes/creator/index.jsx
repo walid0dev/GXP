1cb1a8f9fb050d545ad0a439741334edfa063f8b
ayoubhanine <ayoubhanine2000@gmail.com>
2026-05-07 09:50:37 +0100

    styliser loading

--- src/routes/creator/index.jsx
+++ src/routes/creator/index.jsx
@@ -42,7 +42,9 @@ const fetchCreators=async()=>{
  fetchCreators()
 
 },[page])
-if(loading) return <p>...Loading</p>
+if(loading) return (  <div className="flex justify-center items-center h-screen">
+      <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>)
 if(error) return <p>{error}</p>
   return(
   <div className="min-h-screen bg-[#020617] text-white px-6 py-10">
