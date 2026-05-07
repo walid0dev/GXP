0a3f87722980064400061c45789595054e05fab2
walid-hud <walidthehud@gmail.com>
2026-05-07 12:33:50 +0100

    fix(api): fix api key mismatch

--- services/apiCreators.jsx
+++ services/apiCreators.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const api=axios.create({
-    baseURL:"https://api.rawg.io/api",
-    params:{
-        key:import.meta.env.VITE_API_KEY //le paramètre envoyé à l’API RAWG.
-    }
-})
+const api = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_RAWG_API_KEY, //le paramètre envoyé à l’API RAWG.
+  },
+});
 
 export default api;
+
